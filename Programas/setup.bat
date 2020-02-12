402e25cabb5ca1ebe75569314cb50fdbc60812a2
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2020-02-12 16:19:55 +0100

    Setup actualizado

--- Programas/setup.bat
+++ Programas/setup.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 SET mypath=%~dp0
-setx PATH "%PATH%;%mypath%apache-ant-1.9.14\bin" /m
+For /F "Delims=" %%I In ('echo "%PATH%" ^| find /C /I "%mypath%apache-ant-1.9.14\bin"') Do set pathExists=%%I 2>Nul
+If %pathExists%==0 (setx PATH "%PATH%;%mypath%apache-ant-1.9.14\bin" /m)
 setx CATALINA_HOME "%mypath%Tomcat 8.5" /m
+setx AXIS2_HOME "%mypath%axis2-1.7.9" /m
 xcopy %mypath%axis2.war "%mypath%\Tomcat 8.5\webapps"
